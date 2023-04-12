8a87140521d0762ceaa8a0fd332a135952c42109
Adarsha A <adarshaadi1997@gmail.com>
2023-04-12 17:41:59 +0530

    Except footer everything completed

---
+++ index.js
@@ -0,0 +1,3 @@
+alert('Website under construction... Please wait for utilizing complete features...');
+alert('Please open in Laptop or Computer for better Experience...');
+alert('Made by ADARSHA A')
